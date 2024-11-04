e014226302f9abad0abcd10acbdf1c23dca32a76
daixianhao <xianhaodai@gmail.com>
2024-11-04 19:21:59 +0800

    main entrance update

--- src/src/game/util/ImageResourceUtil.java
+++ src/src/game/util/ImageResourceUtil.java
@@ -2,32 +2,95 @@ package src.game.util;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class ImageResourceUtil {
+
+    /**
+     * Load origin image
+     * @param path image relative path
+     * @return origin image
+     * @throws IOException exception
+     */
+    public static BufferedImage loadImage(String path) throws IOException {
+        return ImageIO.read(new File(path));
+    }
+
+    /**
+     * Proportionally stretch image icon until its width or height equivalent to that of the main window
+     * @param path image relative path
+     * @return stretched full-window image
+     * @throws IOException exception
+     */
     public static BufferedImage loadScaledImage(String path) throws IOException {
-        BufferedImage image = ImageIO.read(new File(path));
-        /*
-        Proportionally stretch image icon until its width or height equivalent to that of the main window
-        */
-        int newHeight = MainWindowUtil.mainWindowHeight - MainWindowUtil.mainWindowInsets.top - MainWindowUtil.mainWindowInsets.bottom;
-        int newWidth = MainWindowUtil.mainWindowWidth - MainWindowUtil.mainWindowInsets.left - MainWindowUtil.mainWindowInsets.right;
-        float stretchScaleOnHeight = newHeight / (float)image.getHeight();
-        float stretchScaleOnWidth = newWidth / (float)image.getWidth();
+        int width = MainWindowUtil.mainWindowWidth - MainWindowUtil.mainWindowInsets.left - MainWindowUtil.mainWindowInsets.right;
+        int height = MainWindowUtil.mainWindowHeight - MainWindowUtil.mainWindowInsets.top - MainWindowUtil.mainWindowInsets.bottom;
+        return ImageResourceUtil.loadScaledImage(path, width, height);
+    }
+
+    /**
+     * Proportionally stretch image icon with a certain scale
+     * @param path image relative path
+     * @param scale resized scale
+     * @return stretched image with certain scale
+     * @throws IOException exception
+     */
+    public static BufferedImage loadScaledImage(String path, float scale) throws IOException {
+        BufferedImage image = loadImage(path);
+        return ImageResourceUtil.loadScaledImage(path, (int) (image.getWidth() * scale), (int) (image.getWidth() * scale));
+    }
+
+    /**
+     * Proportionally stretch image icon until its width or height equivalent to that of the parameter
+     * @param path image relative path
+     * @param width target width
+     * @param height target height
+     * @return stretched image
+     * @throws IOException exception
+     */
+    public static BufferedImage loadScaledImage(String path, int width, int height) throws IOException {
+        BufferedImage image = loadImage(path);
+        float stretchScaleOnWidth = width / (float)image.getWidth();
+        float stretchScaleOnHeight = height / (float)image.getHeight();
         float stretchScale = Math.max(stretchScaleOnHeight, stretchScaleOnWidth);
         if (Float.compare(stretchScale, stretchScaleOnHeight) == 0) {
-            newWidth = (int)(image.getWidth() * stretchScale);
+            width = (int)(image.getWidth() * stretchScale);
         } else if (Float.compare(stretchScale, stretchScaleOnWidth) == 0) {
-            newHeight = (int)(image.getHeight() * stretchScale);
+            height = (int)(image.getHeight() * stretchScale);
         }
-        return ImageResourceUtil.resizeImage(image, newWidth, newHeight);
+        return ImageResourceUtil.resizeImage(image, width, height);
+    }
+
+    /**
+     * Rotate image icon with a certain angle
+     * @param path image relative path
+     * @param angle target angle
+     * @return rotated image
+     * @throws IOException exception
+     */
+    public static BufferedImage loadRotatedImage(String path, int angle) throws IOException {
+        BufferedImage image = loadImage(path);
+        return ImageResourceUtil.rotateImage(image, angle);
+    }
+
+    private static BufferedImage rotateImage(BufferedImage originImage, int angle) {
+        int width = originImage.getWidth();
+        int height = originImage.getHeight();
+        BufferedImage rotatedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = rotatedImage.createGraphics();
+        AffineTransform transform = new AffineTransform();
+        transform.rotate((double) angle / 360, (double) width / 2, (double) height / 2);
+        g2d.drawImage(originImage, transform, null);
+        g2d.dispose();
+        return rotatedImage;
     }
 
     private static BufferedImage resizeImage(BufferedImage originImage, int newWidth, int newHeight) {
         Image image = originImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-        BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
         scaledImage.getGraphics().drawImage(image, 0, 0, null);
         return scaledImage;
     }
--- src/src/game/view/MainEntrancePanel.java
+++ src/src/game/view/MainEntrancePanel.java
@@ -6,16 +6,81 @@ import src.game.util.MainWindowUtil;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class MainEntrancePanel extends JPanel {
+    private final JLabel grassLabel;
+    private final JLabel grassRollLabel;
+    private final int grassRollOriginXPos;
+    private int grassRollXPos;
+    private int grassRollAngle;
+    private final Timer timer;
+
+    public MainEntrancePanel() {
+        // use absolute layout on entrance button
+        setLayout(null);
+        int mainWindowWidth = MainWindowUtil.mainWindowWidth;
+        int mainWindowHeight = MainWindowUtil.mainWindowHeight;
+        JButton entranceButton = new JButton();
+        grassLabel = new JLabel();
+        timer = new Timer(0, _ -> updateRollAnimation(mainWindowWidth / 3));
+        // adjustment of icon and button
+        int leftOffset = 5;
+        int bottomOffset = 23;
+        grassRollLabel = new JLabel();
+        grassRollOriginXPos = mainWindowWidth / 3 - 9 * leftOffset;
+        grassRollAngle = 0;
+        grassRollXPos = grassRollOriginXPos;
+        try {
+            BufferedImage grassImage = ImageResourceUtil.loadScaledImage("src/asset/小组件/LoadBar_grass.png", mainWindowWidth / 3, 0);
+            grassLabel.setIcon(new ImageIcon(grassImage));
+            grassLabel.setBounds(mainWindowWidth / 3 - leftOffset, mainWindowHeight * 3 / 4 - bottomOffset, grassImage.getWidth(), grassImage.getHeight());
+
+            BufferedImage buttonImage = ImageResourceUtil.loadScaledImage("src/asset/小组件/LoadBar_dirt.png", mainWindowWidth / 3, mainWindowHeight / 12);
+            entranceButton.setIcon(new ImageIcon(buttonImage));
+            entranceButton.setBounds(mainWindowWidth / 3, mainWindowHeight * 3 / 4, buttonImage.getWidth(), buttonImage.getHeight());
+            entranceButton.setBorderPainted(false);
+            entranceButton.setFocusPainted(false);
+            entranceButton.setContentAreaFilled(false);
+            entranceButton.setVisible(true);
+
+            ImageIcon grassRollOriginImage = new ImageIcon("src/asset/小组件/关卡/SodRollCap.png");
+            BufferedImage grassRollImage = ImageResourceUtil.loadScaledImage("src/asset/小组件/关卡/SodRollCap.png", grassRollOriginImage.getIconWidth(), grassRollOriginImage.getIconHeight());
+            grassRollLabel.setIcon(new ImageIcon(grassRollImage));
+            grassRollLabel.setBounds(mainWindowWidth / 3 - leftOffset, mainWindowHeight * 3 / 4 - 65, grassRollOriginImage.getIconWidth(), grassRollOriginImage.getIconHeight());
+
+            add(grassRollLabel);
+            add(grassLabel);
+            add(entranceButton);
+            timer.start();
+        } catch (IOException _) {}
+    }
+
+    private void updateRollAnimation(int width) {
+        grassRollXPos += 3;
+        grassRollAngle += 20;
+        if (grassRollXPos > grassRollOriginXPos + width) {
+            timer.stop();
+            return;
+        }
+
+        try {
+            BufferedImage rotatedGrassImage = ImageResourceUtil.loadRotatedImage("src/asset/小组件/关卡/SodRollCap.png", grassRollAngle);
+            grassRollLabel.setIcon(new ImageIcon(rotatedGrassImage));
+            grassLabel.setBounds(grassLabel.getX(), grassLabel.getY(), grassRollXPos - grassRollOriginXPos, grassLabel.getHeight());
+            grassRollLabel.setLocation(grassRollXPos, grassRollLabel.getY());
+            repaint(grassRollLabel.getBounds());
+            repaint(grassLabel.getBounds());
+        } catch (IOException _) {}
+    }
+
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         try {
             BufferedImage titleScreen = ImageResourceUtil.loadScaledImage("src/asset/小组件/关卡/titlescreen.jpg");
             g.drawImage(titleScreen, 0, 0, this);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception _) {
         }
     }
 }
